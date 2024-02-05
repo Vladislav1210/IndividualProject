5420ea8705bfee5b6f203f62f12f5e11113a3694
Fedor Zverev <fdzverev12@gmail.com>
2024-02-05 13:29:46 +0300

    05.02.2024

--- main.py
+++ main.py
@@ -3,21 +3,24 @@ import sys
 
 size = width, height = 1600, 1000
 background_color = (220, 255, 255)
+hint_flag = False
 
 buttons_group = []
 
 
 class Button():
-    def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False):
+    def __init__(self, x, y, width, height, screen, buttonText='Button', onclickFunction=None, button_color='#ffffff'):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.onclickFunction = onclickFunction
-        self.onePress = onePress
+        self.button_color = button_color
         self.alreadyPressed = False
+        self.onePress = False
+        self.surface_pos = screen
         self.fillColors = {
-            'normal': '#ffffff',
+            'normal': button_color,
             'hover': '#666666',
             'pressed': '#333333',
         }
@@ -30,9 +33,9 @@ class Button():
         buttons_group.append(self)
 
     def process(self):
-        mousePos = pygame.mouse.get_pos()
+        mousepos = pygame.mouse.get_pos()
         self.buttonSurface.fill(self.fillColors['normal'])
-        if self.buttonRect.collidepoint(mousePos):
+        if self.buttonRect.collidepoint(mousepos):
             self.buttonSurface.fill(self.fillColors['hover'])
             if pygame.mouse.get_pressed(num_buttons=3)[0]:
                 self.buttonSurface.fill(self.fillColors['pressed'])
@@ -47,11 +50,17 @@ class Button():
             self.buttonRect.width / 2 - self.buttonSurf.get_rect().width / 2,
             self.buttonRect.height / 2 - self.buttonSurf.get_rect().height / 2
         ])
-        screen.blit(self.buttonSurface, self.buttonRect)
+        self.surface_pos.blit(self.buttonSurface, self.buttonRect)
 
 
-def myFunction():
-    print('Button Pressed')
+def hint_show():
+    global hint_flag
+    if hint_flag:
+        hint_flag = False
+    else:
+        hint_flag = True
+        screen.fill(background_color)
+        Button(1500, 30, 80, 80, screen, '?', hint_show)
 
 
 if __name__ == '__main__':
@@ -59,20 +68,24 @@ if __name__ == '__main__':
     font = pygame.font.Font(None, 30)
 
     screen = pygame.display.set_mode(size)
-    screen.fill(background_color)
+
     pygame.display.set_caption('Guitar training simulator')
-    Button(1500, 30, 80, 80, '?', myFunction)
-    image = pygame.image.load('grif_gitari.png')
-    screen.blit(image, (300, 300))
+    screen.fill(background_color)
+    Button(1500, 30, 80, 80, screen, '?', hint_show)
 
     while True:
         pygame.display.flip()
         for event in pygame.event.get():
-
             if event.type == pygame.QUIT:
                 pygame.quit()
 
         for button in buttons_group:
             button.process()
+        if hint_flag:
+            hint_window = pygame.Surface((1200, 800))
+            hint_window.fill('white')
+            screen.blit(hint_window, (200, 100))
+
+
 
 
